a011eb7cedcbf05a9b204cc84bc5209a9cb70da3
DonBatman <serfdon@gmail.com>
2026-01-12 10:05:55 -0800

    Added Lucky Block Support

--- init.lua
+++ init.lua
@@ -69,9 +69,18 @@ minetest.register_craft({
 		{'', '', ''},
 	}
 })
+
+if core.get_modpath("lucky_block") then
+	lucky_block:add_blocks({
+		{"dro", {"mydefaultfences:fence_"..nam}, 10},
+		{"dro", {"mydefaultfences:fence_rail_"..nam}, 10},
+		{"dro", {"mydefaultfences:fencegate_"..nam}, 2},
+	})
 end
 
-if minetest.get_modpath("myores") then
+end
+
+if core.get_modpath("myores") then
 local ores = {
 		{"myores:basalt","Basalt","basalt"},
 		{"myores:gneiss","Gneiss","gneiss"},
@@ -154,5 +163,174 @@ local ores = {
 	})
 	end
 end
+-------------------------------------------------------------------------
+
+if minetest.get_modpath("mywhiteblock") then
+
+mydefaultfences = {}
+
+mydefaultfences.colors = {
+	{"black",      "Black",      "#000000"},
+	{"blue",       "Blue",       "#2000c9"},
+	{"brown",      "Brown",      "#954c05"},
+	{"cyan",       "Cyan",       "#01ffd8"},
+	{"darkgreen", "Dark Green",  "#005b07"},
+	{"darkgrey",  "Dark Grey",   "#303030"},
+	{"green",      "Green",      "#61ff01"},
+	{"grey",       "Grey",       "#5b5b5b"},
+	{"magenta",    "Magenta",    "#ff05bb"},
+	{"orange",     "Orange",     "#ff8401"},
+	{"pink",       "Pink",       "#ff65b5"},
+	{"red",        "Red",        "#ff0000"},
+	{"violet",     "Violet",     "#ab23b0"},
+	{"white",      "White",      "#ffffff"},
+	{"yellow",     "Yellow",     "#e3ff00"},
+}
+
+if core.get_modpath("mydye") then
+	mydefaultfences.colors = {
+	{"black",      	"Black",      		"#000000"},
+	{"blue",       	"Blue",       		"#2000c9"},
+	{"brown",     	"Brown",      		"#954c05"},
+	{"cyan",      	"Cyan",       		"#01ffd8"},
+	{"darkgreen", 	"Dark Green",  		"#005b07"},
+	{"darkgrey",  	"Dark Grey",   		"#303030"},
+	{"green",     	"Green",      		"#61ff01"},
+	{"grey",       	"Grey",       		"#5b5b5b"},
+	{"magenta",    	"Magenta",    		"#ff05bb"},
+	{"orange",     	"Orange",     		"#ff8401"},
+	{"pink",      	"Pink",       		"#ff65b5"},
+	{"red",        	"Red",        		"#ff0000"},
+	{"violet",     	"Violet",     		"#ab23b0"},
+	{"white",      	"White",      		"#ffffff"},
+	{"yellow",     	"Yellow",     		"#e3ff00"},
+	{"peachpuff",	"Peachpuff", 		"#FFDAB9"},
+	{"navy",		"Navy", 			"#000080"},
+	{"coral",		"Coral", 			"#FF7F50"},
+	{"khaki",		"Khaki", 			"#F0E68C"},
+	{"lime",		"Lime", 			"#00FF00"},
+	{"light_pink",	"Light Pink", 		"#FFB6C1"},
+	{"light_grey",	"Light Grey", 		"#D3D3D3"},
+	{"purple",		"Purple", 			"#800080"},
+	{"maroon",		"Maroon", 			"#800000"},
+	{"aquamarine",	"Aqua Marine", 		"#7FFFD4"},
+	{"chocolate",	"Chocolate", 		"#D2691E"},
+	{"crimson",		"Crimson", 			"#DC143C"},
+	{"olive",		"Olive", 			"#808000"},
+	{"white_smoke",	"White Smoke", 		"#F5F5F5"},
+	{"mistyrose",	"Misty Rose", 		"#FFE4E1"},
+	{"orchid",		"Orchid", 			"#DA70D6"},
+	}
+end
+
+local whiteblock = {
+		{"mywhiteblock:block","White Block","white"},
+		}
+	for i in ipairs (whiteblock) do
+		local wo = whiteblock[i][1]
+		local des = whiteblock[i][2]
+		local nam = whiteblock[i][3]
+
+	default.register_fence("mydefaultfences:fence_" .. nam, {
+		description = (des .. " Fence"),
+		texture = "mywhiteblock_"..nam..".png",
+		material = wo,
+		groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2},
+		sounds = default.node_sound_wood_defaults(),
+		check_for_pole = true
+	})
+
+	minetest.register_craft({
+		output = 'mydefaultfences:fence_'..nam..' 2',
+		recipe = {
+			{wo, 'default:stick', wo},
+			{wo, 'default:stick', wo},
+			{'', '', ''},
+			}
+	})
+
+	default.register_fence_rail("mydefaultfences:fence_rail_"..nam, {
+		description = des..(" Fence Rail"),
+		texture = "mywhiteblock_"..nam..".png",
+		inventory_image = "default_fence_rail_overlay.png^mywhiteblock_"..nam..".png^" ..
+					"default_fence_rail_overlay.png^[makealpha:255,126,126",
+		wield_image = "default_fence_rail_overlay.png^mywhiteblock_"..nam..".png^" ..
+				"default_fence_rail_overlay.png^[makealpha:255,126,126",
+		material = wo,
+		groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2},
+		sounds = default.node_sound_wood_defaults()
+	})
+
+	minetest.register_craft({
+		output = 'mydefaultfences:fence_rail_'..nam..' 2',
+		recipe = {
+			{wo, wo, ''},
+			{'', '', ''},
+			{wo, wo, ''},
+			}
+	})
+
+	doors.register_fencegate("mydefaultfences:fencegate_" .. nam, {
+		description = (des .. " Fence Gate"),
+		texture = "mywhiteblock_"..nam..".png",
+		material = wo,
+		groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2}
+	})
+
+	minetest.register_craft({
+		output = 'mydefaultfences:fencegate_'..nam..' 2',
+		recipe = {
+			{'default:stick', wo, 'default:stick'},
+			{'default:stick', wo, 'default:stick'},
+			{'', '', ''},
+			}
+	})
 
 
+for _, entry in ipairs(mydefaultfences.colors) do
+	local color = entry[1]
+	local desc = entry[2]
+	local paint = "^[colorize:"..entry[3]
+
+local paintables = {
+	"mydefaultfences:fence_" .. color, "mydefaultfences:fence_rail_"..color, "mydefaultfences:fencegate_" .. color
+}
+
+	default.register_fence("mydefaultfences:fence_" .. color, {
+		description = (desc .. " Fence"),
+		texture = "mywhiteblock_white.png"..paint,
+		material = wo,
+		groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2},
+		sounds = default.node_sound_wood_defaults(),
+		check_for_pole = true
+	})
+	default.register_fence_rail("mydefaultfences:fence_rail_"..color, {
+		description = des..(" Fence Rail"),
+		texture = "mywhiteblock_"..nam..".png",
+		inventory_image = "default_fence_rail_overlay.png^mywhiteblock_"..nam..".png^" ..
+					"default_fence_rail_overlay.png^[makealpha:255,126,126",
+		wield_image = "default_fence_rail_overlay.png^mywhiteblock_"..nam..".png^" ..
+				"default_fence_rail_overlay.png^[makealpha:255,126,126",
+		material = wo,
+		groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2},
+		sounds = default.node_sound_wood_defaults()
+	})
+		doors.register_fencegate("mydefaultfences:fencegate_" .. color, {
+		description = (des .. " Fence Gate"),
+		texture = "mywhiteblock_"..nam..".png",
+		material = wo,
+		groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2}
+	})
+
+	if core.get_modpath("mypaint") then
+		local colors = {}
+		for _, entry in ipairs(mydefaultfences.colors) do
+			table.insert(colors, entry[1])
+		end
+		mypaint.register(paintables, colors)
+	end
+
+
+end
+end
+end
